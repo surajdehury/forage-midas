e9df4d08fe5169598642958ddfa656cb208ee4fa
Suraj Dehury <surajsonu07@Surajs-MacBook-Air.local>
2026-01-05 15:43:18 +0530

    Added Kafka listener for Task 2

---
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -0,0 +1,17 @@
+package com.jpmc.midascore.kafka;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(
+        topics = "${general.kafka-topic}",
+        groupId = "midas-core-group"
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("RECEIVED AMOUNT: " + transaction.getAmount());
+    }
+}
